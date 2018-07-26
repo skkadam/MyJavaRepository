9f68265a948032e32725f6cf7981d5f19807663d
shraddha kadam <shraddha_kadam@persistent.co.in>
2018-07-26 15:41:19 +0530

    File2 is created

---
+++ HelloWorldGit.java
@@ -0,0 +1,14 @@
+class HelloWorld
+{
+
+	void displayDate()
+	{
+		System.out.println("Display todays date is 25.07.2018");
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hello Persistent .......Welcome to GIT ");
+	}
+ 
+}
